9fff0753732c72bbf4377a68a13f917c7f3780cd
edwinisnow <edwin04@gmail.com>
2023-07-02 07:05:46 +0100

    useNavigate hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>Home</div>
+      <button onClick={() => navigate("order-summary", { replace: true })}>
+        Place order
+      </button>
+    </>
+  );
 }
 
 export default Home;
