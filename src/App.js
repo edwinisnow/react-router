0c948962e43aaa81614cefb626ab9a4d66b9ee34
edwinisnow <edwin04@gmail.com>
2023-07-02 08:14:52 +0100

    Lazy. Suspension

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
 import NoMatch from "./components/NoMatch";
@@ -11,13 +12,24 @@ import NewProduts from "./components/NewProducts";
 import Users from "./components/Users";
 import UserDetails from "./components/UserDetails";
 import Admin from "./components/Admin";
+
+const LazyAbout = React.lazy(() => import("./components/About"));
+
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
+        {/* <Route path="about" element={<About />}></Route> */}
+        <Route
+          path="about"
+          element={
+            <React.Suspense fallback="Loading...">
+              <LazyAbout />
+            </React.Suspense>
+          }
+        ></Route>
         <Route path="order-summary" element={<OrderSummary />}></Route>
         <Route path="products" element={<Products />}>
           <Route index element={<FeaturedProduts />} />
