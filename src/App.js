c79bf941628329c2142247eba4d2ff52631094e5
edwinisnow <edwin04@gmail.com>
2023-07-02 06:49:01 +0100

    Navigation Link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/about" element={<About />}></Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </>
   );
 }
 
